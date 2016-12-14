18146262c447c552c6f76d1fffc89012ba3c7a37
Sergey <peskov.s.b@gmail.com>
2016-12-14 16:54:08 +0300

    joomla start

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-12-14 13:08:01 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-12-14T13:08:01+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
